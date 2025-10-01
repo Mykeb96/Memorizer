0d17ded0f6fd2c194a1575b6e0a8eed64c5817d3
unknown <mykael.barnes@hotmail.com>
2025-10-01 11:50:46 -0700

    feat(components): added card skeleton and scss implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './styles/App.css'
 
+import Card from './components/Card'
+
 function App() {
+  const arr = [
+    1,
+    2,
+    3,
+    4,
+    5,
+  ]
 
   return (
     <>
-
+      {arr.map((el: number, key: number) => <Card key={key} num={el} />)}
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+import styles from '../styles/components/Card.module.scss'
 
+interface CardProps {
+    num: number;
+}
 
-export default function Card() {
+export default function Card({ num }: CardProps) {
+    const [enabled, setEnabled] = useState<boolean>(false);
+
+    const handleState = () => {
+        setEnabled(!enabled);
+    }
+
     return (
-        <div>
-            card
+        <div className={styles.container}>
+            <h5>Card {num}</h5>
+            <button onClick={handleState}>enabled: {enabled.toString()}</button>
         </div>
     )
 }
